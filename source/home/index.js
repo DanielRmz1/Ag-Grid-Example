8af2f7bd953e6644b2951c136bcf44b674b1147b
Daniel Ramirez <Daniel.Ramirez1@ey.com>
2020-10-01 12:41:30 -0500

    Add new examples

--- source/home/index.js
+++ source/home/index.js
@@ -12,61 +12,84 @@ const columnDefs = [
 		checkboxSelection: true,
 		headerCheckboxSelection: true,
 		width: 150,
-		editable: true
+		editable: true,
+		filter: true,
+		sortable: true,
+		sortingOrder: ["desc"]
 	},
-	{ 
-		headerName: "Variable", 
+	{
+		headerName: "Variable",
 		field: "Int_VariableId",
 		width: 100,
-		editable: true
+		editable: true,
+		filter: true,
+		sortable: true
 	},
 	{ 
 		headerName: "Chart", 
 		field: "Chart_Name",
-		editable: true
+		editable: true,
+		filter: true,
+		sortable: true
 	},
 	{ 
 		headerName: "Question Type", 
 		field: "Int_QuestionType",
-		editable: true
+		editable: true,
+		filter: true,
+		sortable: true
 	},
 	{ 
 		headerName: "Chart value", 
 		field: "Chart_Value",
-		editable: true
+		editable: true,
+		filter: true,
+		sortable: true
 	},
 	{ 
 		headerName: "Moments ID", 
 		field: "Int_MomentsId",
-		editable: true
+		editable: true,
+		filter: true,
+		sortable: true
 	},
 	{ 
 		headerName: "Min value", 
 		field: "Float_MinValue",
-		editable: true
+		editable: true,
+		filter: true,
+		sortable: true
 	},
 	{ 
 		headerName: "Max value", 
 		field: "Float_MaxValue",
-		editable: true
+		editable: true,
+		filter: true,
+		sortable: true
 	},
 	{ 
 		headerName: "Min value nominale", 
 		field: "Float_MinValueNominal",
-		editable: true
+		editable: true,
+		filter: true,
+		sortable: true
 	},
 	{ 
 		headerName: "Max value nominale", 
 		field: "Float_MaxValueNominale",
-		editable: true
+		editable: true,
+		filter: true,
+		sortable: true
 	},
 	{ 
-		headerName: "Date", 
+		headerName: "Date",
 		field: "DateTime_Date",
 		editable: true,
 		onCellValueChanged: () => {
 			console.log("ghoo");
-		}
+		},
+		filter: true,
+		sortable: true
 	},
 	{
 		headerName: "Calculation Example",
@@ -77,11 +100,23 @@ const columnDefs = [
 			} = data;
 
 			return Float_MinValue * Float_MaxValue;
-		}
+		},
+		filter: true,
+		sortable: true
 	},
 	{
 		headerName: "Dropdown",
-		cellRenderer: "link"
+		cellRenderer: "renderDropdown",
+		field: "color",
+		cellStyle: ({ data }) => {
+			return {
+				backgroundColor: data.color
+			};
+		}
+	},
+	{
+		headerName: "Buttons",
+		cellRenderer: "deleteIcon"
 	}
 ];
 
@@ -95,8 +130,10 @@ const gridOptions = {
 		changeDeleteButtonState();
 	},
 	components: {
-		link: link
+		renderDropdown: renderDropdown,
+		deleteIcon: deleteIcon
 	},
+	animateRows: true,
 	suppressHorizontalScroll: false,
 	rowData: data
 };
@@ -110,14 +147,20 @@ document.addEventListener('DOMContentLoaded', () => {
 btnDelete.addEventListener("click", () => {
 	const selectedNodes = gridApi.getSelectedNodes();
 
-	for (let i = 0; i < data.length; i++) {
-		const isSelected = selectedNodes.filter(({ data: item }) => { return data[i].Int_Id === item.Int_Id; });
+	data = data.map((node) => {
+		const isSelected = selectedNodes.filter(({ data: item }) => item.Int_Id === node.Int_Id);
 
-		if (isSelected.length)
-			data.splice(i, 1);
-	}
+		if (!isSelected.length) {
+			return node;
+		}
 
-	gridApi.setRowData(data);
+		return {
+			...node,
+			removed: true
+		};
+	});
+
+	updateTable();
 	changeDeleteButtonState(true);
 })
 
@@ -136,13 +179,77 @@ changeDeleteButtonState = (disable) => {
 
 btnAdd.addEventListener("click", () => {
 	const selectedNode = gridApi.getSelectedNodes()[0];
-
-  	var newPrice = Math.floor(Math.random() * 100000);
-  	selectedNode.setDataValue("Float_MinValue", newPrice);
+	const calc = Math.floor(Math.random() * 100000);
+	selectedNode.setDataValue("Float_MinValue", calc);
 })
 
-function link() {
-	const aTag = document.createElement("div");
-	aTag.appendChild(document.createElement("button"));
-	return aTag;
+function renderDropdown(node) {
+	console.log(node);
+	const select = document.createElement("select");
+	select.classList.add("dropdown");
+	select.addEventListener("change", (e) => dropdownChange(e, node));
+
+	select.innerHTML = '<option value = "#FFFFFF">White</option>' +
+	'<option value = "#FF0000" ' + isSelected("#FF0000", node.data.color) + '>Red</option>' +
+	'<option value = "#FFCC00" ' + isSelected("#FFCC00", node.data.color) + '>Orange</option>' +
+	'<option value = "#FFFF00" ' + isSelected("#FFFF00", node.data.color) + '>Yellow</option>' +
+	'<option value = "#00FF00" ' + isSelected("#00FF00", node.data.color) + '>Green</option>' +
+	'<option value = "#0000FF" ' + isSelected("#0000FF", node.data.color) + '>Blue</option>' +
+	'<option value = "#663366" ' + isSelected("#663366", node.data.color) + '>Indigo</option>' +
+	'<option value = "#FF00FF" ' + isSelected("#FF00FF", node.data.color) + '>Violet</option>';
+	return select;
+}
+
+function isSelected(value1, value2) {
+	return value1 == value2 ? "selected" : "";
+}
+
+function deleteIcon(node) {
+	const div = document.createElement("div");
+	const iTag = document.createElement("i");
+	iTag.classList.add("fas");
+	iTag.classList.add("fa-trash");
+	iTag.classList.add("icon");
+
+	iTag.addEventListener("click", () => {
+		deleteRecord(node);
+	});
+
+	const editTag = document.createElement("i");
+	editTag.classList.add("fas");
+	editTag.classList.add("fa-pen");
+	editTag.classList.add("icon");
+
+	editTag.addEventListener("click", () => {
+		deleteRecord(node);
+	});
+
+	div.append(iTag);
+	div.append(editTag);
+
+	return div;
+}
+
+function deleteRecord(node) {
+	data = data.map((x) => {
+		if (x.Int_Id !== node.data.Int_Id) {
+			return x;
+		}
+
+		return {
+			...x,
+			removed: true
+		};
+	});
+
+	updateTable();
+}
+
+function updateTable() {
+	data = data.filter(x => !x.removed);
+	gridApi.setRowData(data);
+}
+
+function dropdownChange(event, { node }) {
+	node.setDataValue("color", event.target.value);
 }
